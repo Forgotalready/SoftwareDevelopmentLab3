93c08bf5e732d17e05c8f8c071a6941103a94f6d
ForgotAlready <oleg.burdakin2016@yandex.ru>
2024-05-30 21:19:52 +0700

    Разобран пример из лекции

--- main.cpp
+++ main.cpp
@@ -1,8 +1,31 @@
+#include <QDir>
+
+#include <QTextStream>
+
 #include <QCoreApplication>
 
 int main(int argc, char *argv[])
 {
-    QCoreApplication a(argc, argv);
+    QCoreApplication app(argc, argv);
 
-    return a.exec();
+    QDir myDir;
+    QString info;
+    myDir.setPath("C:/qtprojects");
+    info = myDir.path();
+    QFileInfo fileInfo(info);
+
+    QTextStream cout(stdout);
+
+    if(fileInfo.isDir()){
+        QDir dir = fileInfo.dir();
+        if(dir.cd(fileInfo.fileName())){
+            foreach (QFileInfo inf, dir.entryInfoList(QDir::Files | QDir::NoDotAndDotDot, QDir::Type)) {
+                QString temp = inf.fileName();
+                cout << inf.fileName() << "---" << inf.size() << Qt::endl;
+            }
+        }
+        dir.cdUp();
+    }
+
+    app.exec();
 }
