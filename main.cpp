56ad42791fc3e6c1a35d77673e1a94d82db12cff
ForgotAlready <oleg.burdakin2016@yandex.ru>
2024-05-31 00:01:30 +0700

    Реализован прототип функции обхода файловой системы

--- main.cpp
+++ main.cpp
@@ -1,31 +1,40 @@
 #include <QDir>
-
-#include <QTextStream>
-
+#include <iostream>
 #include <QCoreApplication>
+#include <vector>
+#include <unordered_map>
+
+void directoryTraversal(QDir directory, std::unordered_map<std::string, long long>& statistic){
+    std::string path = directory.absolutePath().toStdString();
+    std::cout << path << std::endl;
+
+    std::cout << "----------\n";
+
+    statistic[path] = 0;
+
+    std::vector<QDir> inDirectories;
+    foreach(QFileInfo inf, directory.entryInfoList(QDir::AllEntries | QDir::NoDotAndDotDot))
+        if(!inf.isDir()){
+            std::cout << inf.filePath().toStdString() << " " << inf.size() << std::endl;
+            statistic[path] += (static_cast<long long>(inf.size()));
+        }
+        else
+            inDirectories.push_back(QDir(inf.filePath()));
+
+    std::cout << "----------\n" << std::endl;
+    for(QDir& dir : inDirectories)
+        directoryTraversal(dir, statistic);
+}
 
 int main(int argc, char *argv[])
 {
     QCoreApplication app(argc, argv);
 
-    QDir myDir;
-    QString info;
-    myDir.setPath("C:/qtprojects");
-    info = myDir.path();
-    QFileInfo fileInfo(info);
+    std::unordered_map<std::string, long long> statistic;
+    directoryTraversal(QDir::current(), statistic);
 
-    QTextStream cout(stdout);
-
-    if(fileInfo.isDir()){
-        QDir dir = fileInfo.dir();
-        if(dir.cd(fileInfo.fileName())){
-            foreach (QFileInfo inf, dir.entryInfoList(QDir::Files | QDir::NoDotAndDotDot, QDir::Type)) {
-                QString temp = inf.fileName();
-                cout << inf.fileName() << "---" << inf.size() << Qt::endl;
-            }
-        }
-        dir.cdUp();
-    }
+    for(auto x : statistic)
+        std::cout << x.first << " " << x.second << std::endl;
 
     app.exec();
 }
